eb65d0a8b618e9ef1fd7c92603b80f511a6c2e28
Amrullah Azzaky <resuslampung@gmail.com>
2025-01-08 15:32:02 +0700

    init: membuat fungsi perkalian dalam c++

---
+++ belajar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+    cout << "input a: ";
+    cin >> a;
+    cout << endl;
+    cout << "input b: ";
+    cin >> b;
+
+    cout << endl;
+    int multiplication = a * b;
+    cout << "hasil dari perkalian antara a dan b adalah: " << multiplication;
+
+    return 0;
+}
